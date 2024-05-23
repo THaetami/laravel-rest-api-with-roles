a5a26e52fd4d3454f26cb887155e1283eb127910
Thaetami <tatanghaetami.97@gmail.com>
2024-05-23 18:19:39 +0700

    fix | unauthentication response

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -10,8 +10,22 @@ class Authenticate extends Middleware
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      */
-    protected function redirectTo(Request $request): ?string
+    protected function redirectTo(Request $request)
     {
-        return $request->expectsJson() ? null : route('login');
+        return null;
+    }
+
+
+    /**
+     * @param \Illuminate\Http\Request $request
+     * @param array $guards
+     * @return void
+     */
+    protected function unauthenticated($request, array $guards)
+    {
+        abort(response()->json([
+            'status' => 'fail',
+            'message' => 'Unauthenticated.'
+        ], 401));
     }
 }
